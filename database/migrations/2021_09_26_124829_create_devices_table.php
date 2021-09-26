c95a0583f44acdd74d88cd9cf7b7fd768388cb26
ahmedsabry1996 <ahmedmahfouzjob@gmail.com>
2021-09-27 00:32:39 +0300

    Add neccessary files

---
+++ database/migrations/2021_09_26_124829_create_devices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->string('uid');
+            $table->string('app_id');
+            $table->enum('os', ['ios', 'android']);
+            $table->string('language');
+            $table->timestamps();
+
+            $table->unique(['uid', 'app_id', 'os']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
